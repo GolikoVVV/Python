b9b6defbea3c3a80fbfa9a468a3d456466a888bb
GolikoVVV <e-mail-51@yandex.ru>
2025-08-21 17:18:31 +0300

    Исправления

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -3,7 +3,8 @@ catalog = [
         Smartphone(brand="RealMe", model="10", number="+79943543636"),
         Smartphone(brand="Xiaomi", model="13", number="+79309741212"),
         Smartphone(brand="OPPO", model="A5x", number="+79328643737"),
-        Smartphone(brand="POCO", model="F2F", number="+79203845757")
+        Smartphone(brand="POCO", model="F2F", number="+79203845757"),
+        Smartphone(brand="Tecno", model="SPARK", number="+79234859595")
         ]
 for Smartphone in catalog:
     print(f"{Smartphone.brand} - {Smartphone.model}. {Smartphone.number}")
